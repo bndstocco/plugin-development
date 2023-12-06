8337341380a6f3aed3af0970ad087ddd6ee9e5b6
bndstocco <bndst77@gmail.com>
2023-12-06 20:25:15 -0300

    commit

--- app/public/wp-content/plugins/mv-slider/views/mv-slider_metabox.php
+++ app/public/wp-content/plugins/mv-slider/views/mv-slider_metabox.php
@@ -1,11 +1,24 @@
+<?php
+    $meta = get_post_meta($post -> ID);
+    var_dump($meta);
+    $link_url = get_post_meta( $post->ID, 'mv_slider_link_text', true );
+    $link_url = get_post_meta( $post->ID, 'mv_slider_link_url', true );
+    var_dump( $link_text, $link_url);
+?>
+
 <table class="form-table mv-slider-metabox">
     <tr>
         <th>
             <label for="mv_slider_link_text">Link Text</label>
         </th>
         <td>
-            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text"
-                value="">
+            <input type="text" 
+            name="mv_slider_link_text" 
+            id="mv_slider_link_text" 
+            class="regular-text link-text"
+            value="<?php echo (isset($link_text)) ? esc_html ($link_text) : '';?>"
+            required    
+            >
         </td>
     </tr>
     <tr>
@@ -13,7 +26,13 @@
             <label for="mv_slider_link_url">Link URL</label>
         </th>
         <td>
-            <input type="url" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" value="">
+            <input type="url"
+            name="mv_slider_link_url"
+            id="mv_slider_link_url"
+            class="regular-text link-url"
+            value="<?php echo (isset($link_url)) ? esc_url ($link_url) : '';?>"
+            required
+            >
         </td>
     </tr>
 </table>
