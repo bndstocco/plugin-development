51fecbf792369b31c78dfaa6e5db1c423bf10e4a
bndstocco <bndst77@gmail.com>
2023-12-08 19:14:00 -0300

    alters

--- app/public/wp-content/plugins/mv-slider/class.mv-slider-settings.php
+++ app/public/wp-content/plugins/mv-slider/class.mv-slider-settings.php
@@ -1,50 +1,45 @@
 <?php
 
 
-if (!class_exists('MV_Slider_Settings')) {
-    class MV_Slider_Settings
-    {
-        public static $options;
-
-        public function __construct()
+if (is_admin()) {
+    if (!class_exists('MV_Slider_Settings')) {
+        class MV_Slider_Settings
         {
+            public static $options;
 
-            self::$options = get_option('mv_slider_options');
+            public function __construct()
+            {
+                self::$options = get_option('mv_slider_options');
+                add_action('admin_init', array($this, 'admin_init'));
+            }
 
-            add_action('admin_init', array($this, 'admin_init'));
+            public function admin_init()
+            {
+                include_once(ABSPATH . 'wp-admin/includes/plugin.php'); // Certifique-se de incluir o arquivo que contém register_settings
+
+                register_setting('mv_slider_group', 'mv_slider_options');
+                add_settings_section(
+                    'mv_slider_main_section',
+                    'How does it work?',
+                    null,
+                    'mv_slider_page1'
+                );
+
+                add_settings_field(
+                    'mv_slider_shortcode',
+                    'Shortcode',
+                    array($this, 'mv_slider_shortcode_callback'),
+                    'mv_slider_page1',
+                    'mv_slider_main_section'
+                );
+            }
+
+            public function mv_slider_shortcode_callback()
+            {
+                ?>
+                <span>Use the shortcode [mv_slider] to display the slider in any page/post/widget</span>
+                <?php
+            }
         }
-        public function admin_init()
-        {
-            add_settings_section(
-
-                'mv_slider_main_section',
-                'How does it work?',
-                null,
-                'mv_slider_page1'
-
-            );
-
-            add_settings_field(
-
-                'mv_slider_shortcode',
-                'Shortcode',
-                array($this, 'mv_slider_shortcode_callback'),
-                'mv_slider_page1',
-                'mv_slider_main_section'
-
-            );
-        }
-
-        public function mv_slider_shortcode_callback()
-        {
-            ?>
-            <span>Use the shortcode [mv_slider] to display the slider in any page/post/widget</span>
-
-            <?php
-
-        }
-
-
-
     }
 }
